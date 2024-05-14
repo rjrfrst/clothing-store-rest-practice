f71c4050e888f7931dd2fc640657c81a2aaafcca
rjrfrst <rjrfrst@gmail.com>
2024-05-14 14:03:06 +0100

    can get all clothes and tested in postman all good!. Next try to get a single piece of clothing

---
+++ src/main/java/com/fashion/fashionstorerest/controller/ClotheController.java
@@ -0,0 +1,29 @@
+package com.fashion.fashionstorerest.controller;
+
+import com.fashion.fashionstorerest.model.Clothes;
+import com.fashion.fashionstorerest.service.ClotheService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class ClotheController {
+
+    //inside the controller
+    //pass the service
+    @Autowired
+    private ClotheService clotheService;
+
+    //I want to user the CRUD operations here (Get, Post, Put, Delete Mapping)
+
+    //find all clothes
+    @GetMapping("clothes")
+    @ResponseBody //Whatever I am sending is a body (data)
+    public List<Clothes> findAllClothes(){
+        return clotheService.getAllClothes();
+    }
+
+} //last
