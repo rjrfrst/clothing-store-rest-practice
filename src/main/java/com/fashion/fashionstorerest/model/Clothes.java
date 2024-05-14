6d47422adb5ba0270d7ac8d01c50fc2b6f33a573
rjrfrst <rjrfrst@gmail.com>
2024-05-14 13:40:50 +0100

    properties in the model completed

--- src/main/java/com/fashion/fashionstorerest/model/Clothes.java
+++ src/main/java/com/fashion/fashionstorerest/model/Clothes.java
@@ -1,4 +1,23 @@
 package com.fashion.fashionstorerest.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+//using lombok annotations
+@Data
+@NoArgsConstructor //default constructor
+@AllArgsConstructor //Constructor for all properties
+@Component
 public class Clothes {
+
+    //properties
+    private long id;
+    private String clothingType;
+    private int size;
+    private List<String> season;
+
 } //last
